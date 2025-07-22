46504456c65565645144556c629e176bea1beb2c
BadrG <bguellass@gmail.com>
2025-07-22 12:23:17 +0100

    Finished exercice

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let result = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            result = result + word;
+        }
+        return result;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
