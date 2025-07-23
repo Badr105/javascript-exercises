c3afd18e622499253219f64d4749d18593fd0214
BadrG <bguellass@gmail.com>
2025-07-23 12:17:27 +0100

    Finished exercice

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
 const sumAll = function() {
+    let var1 = arguments[0], var2 = arguments[1], sum = 0;
 
+    if (Number.isInteger(var1) && Number.isInteger(var2)) {
+        if (var1 > 0 && var2 > 0) {
+            if (var1 > var2) {
+                [var1, var2] = [var2, var1];
+            }
+            for (let i = var1; i <= var2; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+        else {
+            return "ERROR";
+        }
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
