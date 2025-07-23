cd38a33afdc13884c493d549d63c7dfb10b534d8
BadrG <bguellass@gmail.com>
2025-07-23 11:52:04 +0100

    Finished exercice

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        let value = arguments[i];
+        if (array.includes(value)) {
+            array = array.filter(function(item) {
+                return item !== value;
+            });
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
