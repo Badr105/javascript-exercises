0d462ce526257676aa2dd980f5695ea1035af41c
BadrG <bguellass@gmail.com>
2025-08-11 12:16:21 +0100

    Finished exercice

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
 const add = function() {
-	
+  return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+  let result = 0;
+	for (let i = 0; i < arguments[0].length; i++) {
+    result += arguments[0][i];
+  }
+  return result;
 };
 
 const multiply = function() {
-
+  let result = 1;
+	for (let i = 0; i < arguments[0].length; i++) {
+    result *= arguments[0][i];
+  }
+  return result;
 };
 
 const power = function() {
-	
+	let result = 1;
+	for (let i = 0; i < arguments[1]; i++) {
+    result *= arguments[0];
+  }
+  return result;
 };
 
 const factorial = function() {
-	
+	let result = 1;
+	for (let i = 1; i <= arguments[0]; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
