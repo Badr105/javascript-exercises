a1a933a359da1732f4558fa2f8b5bd7dd45133fd
BadrG <bguellass@gmail.com>
2025-08-11 12:58:02 +0100

    Finished exercice

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
 const palindromes = function () {
+    let word = arguments[0].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g, "").toLowerCase();
+    let flag = 1;
+    for (let i = 0; i < word.length / 2; i++) {
+        if (word[i] !== word[word.length - 1 - i]) {
+            flag = 0;
+            break;
+        }
+    }
 
+    if (flag === 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
