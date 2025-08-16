8ed5c1a0a0c4cbe78daa5d2354d73e5a686b065f
BadrG <bguellass@gmail.com>
2025-08-16 12:03:19 +0100

    Finished exercice

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
 const fibonacci = function() {
+    let order = Number(arguments[0]);
+    
+    if (order < 0) {
+        return "OOPS";
+    }
+    else if (order === 0) {
+        return 0;
+    }
+    else {
+        let set = new Array();
 
+        set.push(1);
+        set.push(1);
+
+        for (let i = 0; i < order - 2; i++) {
+            set.push(set[i] + set[i + 1]);
+        }
+
+        return set[order - 1];
+    }
 };
 
 // Do not edit below this line
